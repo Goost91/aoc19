320e2e64c3f624eddef132bdeb1823f0cbad0e87
Goost91 <goost15@gmail.com>
2019-12-02 02:20:59 +0100

    day 1 solution

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod day1;
+mod common;
+
+// dummy file to fool intellij-rust into thinking the separate files are part of the module
+fn main() {
+    println!("Result of day1.1: {}", day1::part1());
+    println!("Result of day1.2: {}", day1::part2());
+}
